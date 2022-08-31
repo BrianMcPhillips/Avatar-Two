600a57147aa3c56e6c5728a1f6b41c01ca2e1168
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-30 22:03:49 -0400

    Character component

--- src/components/app/Character/Character.jsx
+++ src/components/app/Character/Character.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Character = () => (
-
+const Character = ({ name, photoUrl }) => (
+  <figure>
+    <img src={photoUrl} alt={name} />
+    <figcaption>{name}</figcaption>
+  </figure>
 );
 
 Character.propTypes = {
-
+  name: PropTypes.string.isRequired,
+  photoUrl: PropTypes.string.isRequired
 };
 
 export default Character;
