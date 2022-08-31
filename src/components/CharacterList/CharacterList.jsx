99759753a7c240b22b59ba1e7b4f03dcee3e96a0
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-31 11:52:52 -0400

    CharacterList presentation component

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useCharacters } from '../../hooks/characters';
+import Character from '../Character/Character';
 
 const CharacterList = () => {
-  const { loading } = useCharacters();
+  const { loading, characters } = useCharacters();
+  const characterStuff = characters.map(character => 
+    <li key={character.id}>
+      <Character {...character}/>
+    </li>
+  );
 
   if(loading) return <h1>Loading...</h1>;
+  return (
+    <ul>
+      {characterStuff}
+    </ul>
+  );
 };
 
 export default CharacterList;
--- src/hooks/characters.js
+++ src/hooks/characters.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
+import { getCharacters } from '../services/avatar-api';
 
 export const useCharacters = () => {
   const [loading, setLoading] = useState(true);
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-
+    getCharacters()
+      .then(data => setCharacters(data))
+      .finally(() => setLoading(false));
   }, []);
 
   return {
