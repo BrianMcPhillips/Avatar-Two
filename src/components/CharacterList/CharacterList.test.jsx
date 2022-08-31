730e250f29feeddd462638b1e6a7ac1d1a996539
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-31 10:49:13 -0400

    CharacterList scaffold

---
+++ src/components/CharacterList/CharacterList.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+describe('CharacterList component', () => {
+  it('displays a list of characters after a brief load', () => {
+    
+  });
+});
