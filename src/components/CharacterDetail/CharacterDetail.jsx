5cd5cd66de1905835c5488a531792a08ec0040b3
Brian McPhillips <B.McPhillips89@gmail.com>
2022-09-01 21:12:05 -0400

    Character Detail component finished

--- src/components/CharacterDetail/CharacterDetail.jsx
+++ src/components/CharacterDetail/CharacterDetail.jsx
@@ -4,9 +4,22 @@ import { useSingleCharacter } from '../../hooks/characters';
 
 const CharacterDetail = () => {
   const { id } = useParams();
-  const { loading, data } = useSingleCharacter(id);
+  const { loading, data: {
+    image,
+    name,
+    affiliation,
+    allies
+  } } = useSingleCharacter(id);
 
   if(loading) return <h1>Loading...</h1>;
+  return (
+    <div>
+      <img src={image} alt={name} />
+      <h1>{name}</h1>
+      <h3>{affiliation}</h3>
+      <h4>{allies}</h4>
+    </div>
+  );
 };
 
 export default CharacterDetail; 
