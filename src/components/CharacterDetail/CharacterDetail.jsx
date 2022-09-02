0fe9b0ab32b7c4037f83bf84d6bdd78145216892
Brian McPhillips <B.McPhillips89@gmail.com>
2022-09-01 21:07:30 -0400

    Character detail loading test pass

--- src/components/CharacterDetail/CharacterDetail.jsx
+++ src/components/CharacterDetail/CharacterDetail.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSingleCharacter } from '../../hooks/characters';
 
 const CharacterDetail = () => {
+  const { id } = useParams();
+  const { loading, data } = useSingleCharacter(id);
 
+  if(loading) return <h1>Loading...</h1>;
 };
 
 export default CharacterDetail; 
